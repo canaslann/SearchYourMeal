a65fdc6c6f2bdb0630eaaa07dbaea3e9aa09392f
Can Aslan <134973539+canaslann@users.noreply.github.com>
2024-02-13 20:02:31 +0000

    navbar responsive

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ async function getData() {
 
 getData();
 
+// ---navbar---
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
