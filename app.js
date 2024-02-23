0d058e340d6d8a6a2c36a4082549d8d1bad2b654
ertug <ertug3470@gmail.com>
2024-02-23 23:34:44 +0300

    The codes are added to new responsive menu.

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+// Responsive Navbar
+
+const menuicon = document.querySelector("#bar");
+const crossicon = document.querySelector("#cross");
+
+menuicon.addEventListener("click", openMenu);
+crossicon.addEventListener("click", closeMenu);
+
+function openMenu() {
+  var x = document.querySelector(".nav");
+  if (x.className === "nav") {
+    x.className += "-responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
+function closeMenu() {
+  var x = document.querySelector(".nav-responsive");
+  if (x && x.className === "nav-responsive") {
+    x.className = "nav";
+  } else {
+    console.log("Menü zaten kapalı veya bulunamadı");
+  }
+}
+
+// fetch api
+
 async function getData() {
   const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
   const options = {
@@ -15,18 +43,7 @@ async function getData() {
     .catch((err) => console.error(err));
 }
 
-getData();
-
 // ---navbar---
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
 
 function scrollToSection(sectionId) {
   var section = document.getElementById(sectionId);
