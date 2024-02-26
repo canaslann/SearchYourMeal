86192b9dfaa15c554c64e0c705b74e7a92cb927c
ertug <ertug3470@gmail.com>
2024-02-27 00:29:05 +0300

    The structure of results page built.

--- searchResults.js
+++ searchResults.js
@@ -1,3 +1,31 @@
+// Responsive Navbar
+
+const menuicon = document.querySelector("#bar");
+const crossicon = document.querySelector("#cross");
+
+menuicon.addEventListener("click", openMenu);
+crossicon.addEventListener("click", closeMenu);
+
+function openMenu() {
+  var x = document.querySelector(".nav");
+  if (x.className === "nav") {
+    x.className += "-responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
+function closeMenu() {
+  var x = document.querySelector(".nav-responsive");
+  if (x && x.className === "nav-responsive") {
+    x.className = "nav";
+  } else {
+    console.log("Menü zaten kapalı veya bulunamadı");
+  }
+}
+
+// Requests
+
 const inputvalue = sessionStorage.getItem("searchvalue");
 console.log(inputvalue);
 window.addEventListener("load", () => getData(inputvalue));
@@ -16,11 +44,88 @@ async function getData(inputValue) {
     }
 
     const data = await response.json();
-
+    console.log(data);
     data.meals.forEach((meal) => {
       console.log(meal);
+      renderSearchResults(meal);
     });
   } catch (error) {
     console.error("Error fetching data:", error);
   }
+}
+
+// Render Results
+
+const container = document.querySelector(".results-container");
+
+function renderSearchResults(meals) {
+  meals.forEach((meal) => {
+    const mealDiv = document.createElement("div");
+    mealDiv.classList.add("meal-container");
+
+    renderIngredients(mealDiv, meal);
+    renderRecipes(mealDiv, meal);
+
+    container.appendChild(mealDiv);
+  });
+}
+
+function renderSearchResults(meal) {
+  renderIngredients(meal);
+  renderRecipes(meal);
+}
+
+function renderIngredients(mealData) {
+  const containerLeft = document.querySelector(".container-left");
+
+  // Extract ingredients from mealData
+  const ingredients = [];
+  for (let i = 1; i <= 11; i++) {
+    const ingredient = mealData[`strIngredient${i}`];
+    if (ingredient) {
+      ingredients.push(ingredient);
+    }
+  }
+
+  // Render HTML dynamically
+  containerLeft.innerHTML = `
+    <div class="random-image">
+      <img src="${
+        mealData.strMealThumb
+      }" alt="a Reuben sandwich on wax paper" />
+    </div>
+    <h2 class="container-left-title">Ingredients</h2>
+    <div class="ingredients">
+      <ul>
+        ${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+      </ul>
+   
+  `;
+}
+
+function renderRecipes(mealdata) {
+  const containerRight = document.querySelector(".container-right");
+
+  var videoURL = `${mealdata.strYoutube}`;
+  var splited = videoURL.split("v=");
+  var splitedAgain = splited[1].split("&");
+  var videoId = splitedAgain[0];
+
+  containerRight.innerHTML =
+    containerRight.innerHTML = `<div class="text-container">
+              <h2  class="right-title">${mealdata.strMeal}</h2>
+              <div class="recipe">
+                <h5>Recipe:</h5>
+
+                ${mealdata.strInstructions}
+                <h5>Category:</h5>
+                <p>${mealdata.strCategory}</p>
+<div>
+  <iframe frameborder="0"  width="100%" 
+    src="https://youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&autohide=1">
+  </iframe>
+</div>
+               
+              </div>
+            </div> `;
 }
